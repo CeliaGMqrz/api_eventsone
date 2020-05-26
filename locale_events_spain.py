3bf98bb8dd4873cc9f06aee322fb14a4311ffd5c
Celia Garcia <cg.marquez95@gmail.com>
2020-05-26 15:01:53 +0200

    Añadido programa consulta sitios de eventos

---
+++ locale_events_spain.py
@@ -0,0 +1,45 @@
+#Programa en python que devuelve la sala y el lugar donde se celebran eventos internacionales en España.
+
+#Para ello necesitas autentificarte con la API key.
+# Esta API utiliza la respuesta json.
+
+# Importante!!
+# Si queremos subir este programa a github, debería buscar algún mecanismo para no guardar nuestra key en el repositorio público de github.
+# Para ello vamos a usar variables de entorno.
+
+# Deremos exportar la clave de nuestra cuenta en una variable de entorno desde la terminal:
+# export key ="**************************"
+
+#Lo primero es importar la librería requests
+import requests
+#Importamos la libreria json
+import json
+#Importar la librería os que va leer nuestra variable de entorno
+import os
+
+#Guardamos la url base
+url_base="https://app.ticketmaster.com/discovery/v2/"
+
+#En una variable key, guardamos por el diccionario os.environ nuestra key
+key=os.environ["apikey"]
+code='ES'
+#Vamos a crear un diccionario que guarde nuestros parámetros
+payload = {'apikey':key,'countryCode':code}
+
+#Hacemos la petición, guardandola en una variable r, añadiendo los parametros que necesitamos usando params e indicando el diccionario creado previamente.
+r=requests.get(url_base+'venues.json',params=payload)
+
+#Para asegurarnos que no hay errores consultamos el estado de la petición.
+salas=[]
+lugares=[]
+if r.status_code == 200:
+    #Guardamos el contenido en una variable leido por json.
+    doc = r.json()
+    for lugar in doc["_embedded"]["venues"]:
+        salas.append(lugar["name"])
+        lugares.append(lugar["state"]["name"])
+    filtro=[salas,lugares]
+    
+    for sala,lugar in zip(filtro[0],filtro[1]):
+        print("SALA: ",lugar,"\nLUGAR:",sala)
+
